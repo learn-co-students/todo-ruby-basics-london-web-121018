dc1a5e79674f3388771166770d3311b5a7ee6115
I_AM Elaine <elaine.onb@gmail.com>
2018-11-25 23:51:49 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ num = num1/ num2
+ return num
 end
 
 def assign_variable(value)
-  
+  val = value
+  return val
 end
 
-def argue
-  
+def argue(val)
+  return val
 end
 
-def greeting
-
+def greeting(greeting, name)
+  return "#{greeting} #{name}"
 end
 
-def return_a_value
-
+def return_a_value(argval="Nice")
+  return argval
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(nval="expert")
+  return nval
 end
 
-def pizza_party
-
+def pizza_party(food = "cheese")
+  return food;
 end
